d0dbb7cd41c389dca803962cae4ac5b1cc1c49ba
Abhinav <abhiasthana76@gmail.com>
2025-10-22 09:45:24 +0530

    update: add user sessions and simulate jquery

--- src/basket/basket.py
+++ src/basket/basket.py
@@ -10,11 +10,27 @@ class Basket:
 
         # if there's already a session key then
         # retrieve the data and key
-        basket = self.session.get("s_key")
+        basket = self.session.get("skey")
 
         # if there's no existing session stored in cookies
         # create a session based on the request
-        if "s_key" not in request.session:
-            basket = self.session["skey"] = {"number": 1352352}
+        if "skey" not in request.session:
+            basket = self.session["skey"] = {}
 
         self.basket = basket
+
+    def add(self, product, qty):
+        """Adding and updating the users basket session data."""
+
+        product_id = str(product.id)
+        if product_id not in self.basket:
+            self.basket[product_id] = {
+                "price": str(product.price),
+                "qty": int(qty),
+            }
+
+        self.session.modified = True
+
+    def __len__(self):
+        """Get the basket data and count of items."""
+        return sum(item["qty"] for item in self.basket.values())
--- src/basket/views.py
+++ src/basket/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from store.models import Product
+from .basket import Basket
 
 
 # Create your views here.
 def basket_summary(request):
     return render(request, "basket/summary.html")
+
+
+def basket_add(request):
+    basket = Basket(request)
+
+    if request.method == "POST" and request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+
+        basket_qty = basket.__len__()
+        return JsonResponse(
+            {"success": True, "basket": request.session.get("skey"), "qty": basket_qty}
+        )
+    return JsonResponse({"error": "invalid request"}, status=400)
