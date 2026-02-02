253fc74bfa4b635b6b4bb6eb10dcb6aa9d66ce41
Abhinav <abhiasthana76@gmail.com>
2026-02-02 05:55:31 +0530

    feat(debug): add debug toolbar

--- src/core/urls.py
+++ src/core/urls.py
@@ -19,6 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -32,3 +34,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += debug_toolbar_urls()
